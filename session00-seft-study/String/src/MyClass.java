d6bb7a1f7edd0ece53afbe82c04bf1aed4ad7909
ChiKien_Luu <luukien2910@gmail.com>
2024-11-25 12:23:57 +0700

    String and console

---
+++ session00-seft-study/String/src/MyClass.java
@@ -0,0 +1,43 @@
+
+/**
+ * @author Chi Kien-Luu | github/luwukien
+ * Date: 11/25/2024
+ */
+
+
+public class MyClass {
+    public static void main(String[] args) {
+        //Repeat method
+//        String sureSixTimes = "Sure!".repeat(6);
+//        System.out.println("Are you sure this information? " + sureSixTimes);
+
+        //Cutting String
+//        String str1 = "I love FPTU";
+//        String str2 = str1.substring(7);
+//        String str3 = str1.substring(0, 7);
+//        String str4 = str3.concat("PTIT");
+//        System.out.println(str1);
+//        System.out.println(str4);
+
+        //UpperCase and LowerCase
+//        String rootStr = "clean code";
+//        String rootStr2 = "BAD coDe";
+//        rootStr = rootStr.toUpperCase();
+//        rootStr2 = rootStr2.toLowerCase();
+//        System.out.println(rootStr);
+//        System.out.println(rootStr2);
+
+        //trim()
+        //this method helps you cut the space the first string and the last string
+//        String rootStr = "                 clean code ";
+//        System.out.println(rootStr.trim());
+        //startsWith() and endsWith(): to check the first String/ the last String has a String which you need
+        //charAt(): return index of char which you want to find
+        //replace(): console string.replace(string1, string 2);
+
+//        String rootStr= "Hello World! Hello Yellow Code Books!";
+//        rootStr = rootStr.replace("j", "Hi");
+//        System.out.println(rootStr);
+        //indexOf(): to find the first index of the string char
+    }
+}
